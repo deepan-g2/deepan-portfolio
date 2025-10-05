7500e3262e788c9ff799d34a7a696562d322c97a
deepan-g2 <dkumar@g2.com>
2025-10-05 20:54:11 +0530

    email fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,14 +43,14 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Logo
+          Home
         </a>
         <a
-          href="mailto:example@mail.com"
+          href="mailto:deepan.ppgit@gmail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          example@mail.com
+          deepan.ppgit@gmail.com
         </a>
         <ul>
           <li>
